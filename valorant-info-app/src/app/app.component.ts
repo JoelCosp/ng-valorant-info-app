97c1b181ee60219da688e98852c9145e642218f3
JoelVRV <171981845+JoelCosp@users.noreply.github.com>
2025-04-06 18:18:28 +0200

    INICIO FRONT HOME PAGE

--- valorant-info-app/src/app/app.component.ts
+++ valorant-info-app/src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Pages
+import { HomeComponent } from './pages/home/home.component';
+import { AgentsComponent } from './pages/agents/agents.component';
+import { MapsComponent } from './pages/maps/maps.component';
+import { WeaponsComponent } from './pages/weapons/weapons.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NewsComponent } from './pages/news/news.component';
+
+// Components
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, FooterComponent, HomeComponent, AgentsComponent, MapsComponent, WeaponsComponent, AboutComponent, NewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
