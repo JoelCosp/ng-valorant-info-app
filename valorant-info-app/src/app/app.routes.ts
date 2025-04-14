34a901f368b7238223bade999ffe0bd3fb5a83e9
JoelVRV <171981845+JoelCosp@users.noreply.github.com>
2025-04-14 12:56:55 +0200

    APLICANDO ROUTER LINK EN AGENTES

--- valorant-info-app/src/app/app.routes.ts
+++ valorant-info-app/src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { AboutComponent } from './pages/about/about.component';
 import { AgentsComponent } from './pages/agents/agents.component';
 import { MapsComponent } from './pages/maps/maps.component';
 import { WeaponsComponent } from './pages/weapons/weapons.component';
+import { AgentDetailsComponent } from './pages/agent-details/agent-details.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'agents', component: AgentsComponent },
+    { path: 'agents/:id', component: AgentDetailsComponent },
     { path: 'maps', component: MapsComponent },
     { path: 'weapons', component: WeaponsComponent },
     { path: 'about', component: AboutComponent },
