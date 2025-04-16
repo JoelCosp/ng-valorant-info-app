2ab225e0e6887c382555e54e3da777089445580b
JoelDAW2 <joelc456789@gmail.com>
2025-04-17 00:29:09 +0200

    TABS

--- valorant-info-app/src/app/components/agent-details-section/agent-details-section.component.ts
+++ valorant-info-app/src/app/components/agent-details-section/agent-details-section.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-agent-details-section',
-  imports: [],
+  imports: [MatTabsModule, CommonModule],
   templateUrl: './agent-details-section.component.html',
   styleUrl: './agent-details-section.component.css'
 })
