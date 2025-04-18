6ff74d5995a418b1377c9566a4b7266c429677a3
JoelVRV <joelc456789@gmail.com>
2025-04-18 12:41:04 +0200

    MEJORAS NAV Y HOME PAGE

--- valorant-info-app/src/app/components/nav/nav.component.ts
+++ valorant-info-app/src/app/components/nav/nav.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RedButtonComponent } from '../red-button/red-button.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, NavigationEnd, Router } from '@angular/router';
+import {CommonModule} from "@angular/common"
+import { filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-nav',
-  imports: [RedButtonComponent, RouterLink],
+  imports: [RedButtonComponent, RouterLink, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  navActive!: boolean;
+
+  constructor(private router: Router) {
+    this.navActive = false;
+  }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd) // solo cuando termina la navegación
+      )
+      .subscribe((event: NavigationEnd) => {
+        console.log('Cambié de página a:', event.urlAfterRedirects);
+        this.toggleNav();
+      });
+  }
+
+  toggleNav(): void {
+    this.navActive = !this.navActive;
+  }
 
 }
